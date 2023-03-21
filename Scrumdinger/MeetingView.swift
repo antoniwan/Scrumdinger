9e84fa829e09835d359901c80df52d78b90dd8ec
Antonio Rodriguez <antoniwan@icloud.com>
2023-03-21 13:22:24 -0400

    Test Commit - 🔥

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Antonio Rodriguez on 3/21/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 7, total: 15)
+            HStack {
+                VStack(alignment: .leading){
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack (alignment: .trailing){
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+            }
+                
+        }
+        
+        
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
+
