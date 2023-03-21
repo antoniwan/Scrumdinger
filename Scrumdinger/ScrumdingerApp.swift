77c7d0b1cf1becdc8313d6eaf37ad79786fddf6b
Antonio Rodriguez <antoniwan@icloud.com>
2023-03-21 12:26:16 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Antonio Rodriguez on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
