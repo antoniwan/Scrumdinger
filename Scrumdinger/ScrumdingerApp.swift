0ef007a9772cdf2e8e2ef96a5a152c730316a71b
Antonio Rodriguez <antoniwan@icloud.com>
2023-03-22 16:19:45 -0400

    Tutorial Progress

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    @State private var scrums = DailyScrum.sampleData
+    @State private var scrums = DailyScrum.sampleData // initial state of the app!
     
     var body: some Scene {
         WindowGroup {
